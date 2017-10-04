c68b6d438fbf52ee4b806fbfc1951f9a3d943ba7
mihsan <mihsan191@gmail.com>
2017-10-04 23:29:19 +1100

    Generating containers with connect() from reacth redux (FooterLink)

--- src/index.js
+++ src/index.js
@@ -80,40 +80,34 @@ const Link = ({
   );
 };
 
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return(
-      <Link
-          active={props.filter === state.visibilityFilter}
-          onClick={() => 
-            store.dispatch({
-              type: 'SET_VISIBILITY_FILTER',
-              filter: props.filter
-            })
-          }
-      >
-        {props.children}
-      </Link>
-    );
-  }
-}
-
-FilterLink.contextTypes = {
-  store: PropTypes.object
+const mapStateToLinkProps = (
+  state,
+  props
+) => {
+  return {
+    active:
+      props.filter === state.visibilityFilter
+  };
 };
+
+const mapDispatchToLinkProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      });
+    }
+  };
+};
+
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link);
 
 const Footer = () => (
   <p>
